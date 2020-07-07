080aab97f509d7fc831f15995f9922bc1c896820
prateekganguly1998 <gangulyprateek0@gmail.com>
2020-07-07 10:51:17 +0530

    push3

--- db/db.js
+++ db/db.js
@@ -1,17 +1,22 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("bugbytedb", "root", "", {
-    host: "localhost",
-    port: "3306",
-    dialect: "mysql",
-    operatorAliases: false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-});
+const sequelize = new Sequelize(
+    "d6m5vr5j68tu7i",
+    "mqlwrlelunkrto",
+    "fa90af1255c97ada82159a3b47faf5d54df8471ab795e6a7c47323eea87ae420",
+    {
+        host: "ec2-52-200-48-116.compute-1.amazonaws.com",
+        port: "5432",
+        dialect: "postgres",
+        operatorAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+    }
+);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 module.exports = db;
