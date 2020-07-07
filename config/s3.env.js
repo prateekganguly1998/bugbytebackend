edb573e79e248aae4dce1175034c3bdd44be99cc
prateekganguly1998 <gangulyprateek0@gmail.com>
2020-07-07 16:54:11 +0530

    final push

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,7 +1,7 @@
 const env = {
     Bucket: "bugbyte",
-    AWS_ACCESS_KEY: "AKIAILXPZUGIH4B4NSUA",
-    AWS_SECRET_ACCESS_KEY: "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",
+    AWS_ACCESS_KEY: "",
+    AWS_SECRET_ACCESS_KEY: "",
     ARN: "arn:aws:iam::180732700915:user/prateekganguly1998",
     ARN_ROLE: "arn:aws:iam::180732700915:role/SNSSuccessFeedback",
     REGION: "ap-south-1",
